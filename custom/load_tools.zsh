d66ff01cfe8b0197a21544502f53d11f71a26ce0
Jimmy <jimmy.ilws@gmail.com>
2015-12-15 14:42:09 -0300

    added custom

---
+++ custom/load_tools.zsh
@@ -0,0 +1,42 @@
+HDF5DIR='$HOME/hdf5-1.8.12-linux-x86_64-shared'
+
+# sourceamos el ROOT //// NO FUNCIONA CON ZSH :'(
+# NOTA: para q funciona con el ZSH, le agregue los "cd ..."
+dir_pwd=$PWD
+cd $HOME/root
+source $HOME/root/bin/thisroot.sh
+cd $dir_pwd
+#
+##
+#
+# sourceamos el CDAS (17/mar/2014)
+#
+export WORK=$HOME
+export CDAS=$WORK/CDAS
+CDASVERSION=$(basename $CDAS/version_cdas-*.tgz .tgz | sed -e 's/version_cdas-//' | sed -e 's/p[0-9]//')
+export CMTVERSION=$(basename $CDAS/CMT/*)
+export CMTROOT=$CDAS/CMT/$CMTVERSION
+export CMTINSTALLAREA=$CDAS/
+source ${CMTROOT}/mgr/setup.sh
+source $CDAS/Work/$CDASVERSION/cmt/cdas.sh
+export CMTPATH=$CDAS/:$CDAS/IoFd:$CMTPATH:$WORK
+
+# for Anaconda (python packages):
+export ANACONDA=$HOME/anaconda
+export PATH=$ANACONDA/bin:$PATH
+#export PATH=${HOME}/.local/bin${PATH:+:$PATH}
+#export DYLD_LIBRARY_PATH=/usr/local/lib:$ROOTSYS/lib:$ANACONDA/lib
+#ulimit -n 2048
+
+#------- hdf5 (12.jul.2015)
+export PATH=/usr/local/hdf5/bin:$PATH
+
+source $HOME/magnetocosmics/setupMAGNETOCOSMICS.sh
+export DROP=$HOME/Dropbox
+
+#------- CDF utilities (11.jul.2015, JIMMY :P)
+#source /usr/local/cdf/bin/definitions.B  # esto es para leer .cdf (para "forbush" aun no lo configure (22.sep.2015)
+
+#alias zzz='ls $HOME/*py'
+
+
